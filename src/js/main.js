eb375daff5423a6a655dc4f029ded8dd1d5e121c
butkodenis <Butko.dv@gmail.com>
2017-07-07 14:54:15 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+$(function () {
+    //Идентификатор элемента HTML (например: #datetimepicker1), для которого необходимо инициализировать виджет "Bootstrap datetimepicker"
+    $('#datetimepicker1').datetimepicker();
+});
